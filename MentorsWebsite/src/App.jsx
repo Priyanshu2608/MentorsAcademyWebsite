5a51a3eca5f19435499db2c80dcffbccfc5379f3
Priyanshu Vasudev <142427709+Priyanshu2608@users.noreply.github.com>
2024-12-04 10:59:31 +0530

    footer

--- MentorsWebsite/src/App.jsx
+++ MentorsWebsite/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import Header from './Components/Header'
+import Footer from './Components/Footer'
 
 const App = () => {
   return (
     <div>
       <Header/>
       <Navbar/>
+      <Footer/>
+
     </div>
   )
 }
---
+++ MentorsWebsite/src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <h5 className='Foot'>
+        &copy;Copyright Mentors Academy, Amritsar
+      </h5>
+    </div>
+  )
+}
+
+export default Footer
