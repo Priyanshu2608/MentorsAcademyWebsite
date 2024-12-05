250b24dc0820e0dc5e125396a3aa94ac72ce7001
Priyanshu Vasudev <142427709+Priyanshu2608@users.noreply.github.com>
2024-12-05 18:52:56 +0530

    about

--- MentorsWebsite/src/Components/About.jsx
+++ MentorsWebsite/src/Components/About.jsx
@@ -5,7 +5,11 @@ const About = () => {
   return (
     <div>
       <div className="vidabt">
-        <video src={abt} controls className='vid3'></video>
+        <video src={abt}  autoPlay className='vid3'></video>
+        <div className="about">
+          <h2 className="aboutline">About Our Faculty</h2>
+          <h3 className="quote">"Teachers plant seeds of knowledge that grow forever."</h3>
+        </div>
       </div>
     </div>
   )
